ad0aeb4e2bb6e9dfc885088e79eb6ff43595e912
master-node <thai@master-node.cluster.local>
2025-03-18 21:32:24 -0700

    delete some comments

--- pkg/fukabunsan/algorithm.go
+++ pkg/fukabunsan/algorithm.go
@@ -10,20 +10,10 @@ import (
 
 func (lb *LoadBalancer) LBAlgorithm(lbRequest *LBRequest) *LBResponse {
 	bonalib.Info("LBAlgorithm", "lbRequest", lbRequest)
-	// bonalib.Log("target lists", lbRequest.Targets)
-	// bonalib.Log("NODENAMES", NODENAMES)
-	// bonalib.Log("PodCidr", PODCIDRS)
 
 	// GET NODE SOURCE
 	node_source := RequestfromNode(strings.Split(lbRequest.SourceIP, ":")[0])
-	// bonalib.Log("node_source", node_source)
 	node_sourceidx := NODEIDX[node_source]
-	// bonalib.Log("node source idx", node_sourceidx)
-
-	// GET MIPORIN MATRIX
-	// bonalib.Log("Miporin matrix", MIPORIN_MATRIX)
-	// node_des_list := MIPORIN_MATRIX[node_sourceidx]
-	// bonalib.Log("node destination matrix", node_des_list)
 
 	// FIND RANDOM NODE
 	var node_des_idx int
@@ -36,8 +26,6 @@ func (lb *LoadBalancer) LBAlgorithm(lbRequest *LBRequest) *LBResponse {
 			break
 		}
 	}
-	// bonalib.Log("Node des idx", node_des_idx)
-	// bonalib.Log("Node des", NODENAMES[node_des_idx])
 
 	// FIND ALL PODS IP ADDRESS ON THE NODE SELECTED
 	var selected_targets []string
@@ -46,7 +34,6 @@ func (lb *LoadBalancer) LBAlgorithm(lbRequest *LBRequest) *LBResponse {
 			selected_targets = append(selected_targets, target)
 		}
 	}
-	// bonalib.Log("selected target", selected_targets)
 
 	// SELECT A POD TO DIRECT TRAFFIC TO
 	var (
@@ -63,8 +50,7 @@ func (lb *LoadBalancer) LBAlgorithm(lbRequest *LBRequest) *LBResponse {
 	bonalib.Log("final target", final_target)
 
 	ret := &LBResponse{
-		Target: final_target,
-		// Target:  lbRequest.Targets[0],
+		Target:  final_target,
 		Headers: make([]*LBResponse_HeaderSchema, 0),
 	}
 	ret.Headers = append(ret.Headers, &LBResponse_HeaderSchema{
